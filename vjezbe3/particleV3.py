6a2829d6a11ba833decf338a98fe517001bae6bd
BrunaMariani <bmariani@pmfst.hr>
2021-03-22 13:33:36 +0100

    vj3-prt

--- vjezbe3/particleV3.py
+++ vjezbe3/particleV3.py
@@ -7,6 +7,8 @@ class Particle:
         self.y0 = y0
         self.x_ = []
         self.y_ = []
+        self.y = 0
+        self.x = 0
     def printInfo(self):
         print(f'{self.v0},{self.theta},{self.x0},{self.y0}')
     def reset(self):
@@ -17,33 +19,43 @@ class Particle:
     
     def __move(self, dt):
         g = 9.81
-        y = self.y0
-        x = self.x0
+        
+        
+    
         theta = radians(self.theta)
         v_x = self.v0 * cos(self.theta)
-        x = self.x0 + v_x * dt
+        self.x = self.x0 + v_x * dt
         v_y = self.v0*sin(self.theta)
         v_y = v_y - g*dt
-        y =  self.y0 + v_y*dt
-        self.x_.append(x)
-        self.y_.append(y)
+        self.y =  self.y0 + v_y*dt
+        self.x_.append(self.x)
+        self.y_.append(self.y)
+    def range(self):
        
+        
+
+        
+            
+            
+        
+
+       
+        
+
         
        
 
        
 
-    def range(self):
-        self.__move(0.5)
-             
-        print(x)
+    
+        
              
       
             
-
 p1 = Particle(30,45,2,2)
 p1.printInfo()
 p1.range()
+
 
 
 
