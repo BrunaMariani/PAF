1e2d12cd1d0c1ad4071f25f2f6b1772c85fca800
BrunaMariani <bmariani@pmfst.hr>
2021-04-15 11:00:15 +0200

    zdk3-1

---
+++ vjezbe6/zadatak3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+int main(){
+    int polje[20]= {-10,2,34,11,-32,13,56,-23,43,1,0,-5,4,14,-15,16,-17,18,-19,20};
+    int a;
+    a = -5;
+    int b;
+    b = 15;
+    for (int i = 0; i<21; i++)
+    { 
+        if (polje[i] >= a){
+            if (polje[i] <= b){
+            std::cout << polje[i] << std::endl;
+            }
+        }
+        
+    }
+    return 0;
