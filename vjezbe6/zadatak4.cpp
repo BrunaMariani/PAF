1a9bde1e3de77b42029963a1145e1815754a6a4a
BrunaMariani <bmariani@pmfst.hr>
2021-04-25 13:25:02 +0200

    4. zadatak

---
+++ vjezbe6/zadatak4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+
+float jednadzba(float a1,float b1, float c1,float a2,float b2, float c2)
+{
+    float x; 
+    x = (b1*c2-b2*c1)/(b1*a2-a1*b2);
+    float y;
+    y = (c1-a1*x)/b1;
+   std::cout << "x je:" << x << "y je:" << y << std::endl;
+    
+}
+
+int main()
+{
+    jednadzba(5,2,6,4,-3,7);
