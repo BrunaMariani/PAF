3f43f453471c54c74d297e1881ea74a4fc6d179a
BrunaMariani <bmariani@pmfst.hr>
2021-04-29 09:40:42 +0200

    vj7

--- vjezbe7/Particle.cpp
+++ vjezbe7/Particle.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <math.h>
 
 class Particle{
-    float brzina;
-    float kut;
-    float xkoor;
-    float ykoor;
+    public:
+        Particle(){
+        float brzina;
+        float kut;
+        float xkoor;
+        float ykoor;
+        float pi = 3.14159265;
+        float g = 9.81;
+        float kut = kut*pi/180;
+        }
+
+        float range(brzina, kut, xkoor, ykoor, float dt){
+            float pi = 3.14159265;
+            float g = 9.81;
+            float kut = kut*pi/180;
+            float vx;
+            float vy0;
+            vy0 = brzina*sin(kut);
+            vx = brzina*cos(kut);
+            float vy;
+            vy = vy0;
+            float y = ykoor;
+            float x = xkoor;
+            vy = vy0;
+            while (y>=0){
+            x =x + vx*dt;
+            vy = vy - g*dt;
+            y = y + vy*dt;
+            }
+
+            cout << x-xkoor ;
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
 
