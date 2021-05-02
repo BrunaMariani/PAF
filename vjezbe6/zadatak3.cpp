de2df05bdabd0a1b989bfeece2e28459d8a77672
BrunaMariani <bmariani@pmfst.hr>
2021-05-02 22:35:23 +0200

    zdk3

--- vjezbe6/zadatak3.cpp
+++ vjezbe6/zadatak3.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <cmath>
+
 void ispis(int polje[], int a, int b){
-    int n = sizeof(polje);
-    for (int i = 0; i<21; i++)
+    for (int i = 0; i<20; i++)
     { 
         if (polje[i] >= a){
             if (polje[i] <= b){
@@ -11,22 +11,23 @@ void ispis(int polje[], int a, int b){
         }
         
     }
-
-
+}
+void zamjena_dva(int polje[], int a, int b){
+    int x = polje[a];
+    polje[a]=polje[b];
+    polje[b]= x;
+}
+    
+void elementi(int polje[]) {
+        for ( int i = 0; i<20; i++){
+        std::cout << polje[i] << std::endl;
+    }
+}
 
 int main(){
     int polje[20]= {-10,2,34,11,-32,13,56,-23,43,1,0,-5,4,14,-15,16,-17,18,-19,20};
-    int a;
-    a = -5;
-    int b;
-    b = 15;
-    for (int i = 0; i<21; i++)
-    { 
-        if (polje[i] >= a){
-            if (polje[i] <= b){
-            std::cout << polje[i] << std::endl;
-            }
-        }
-        
-    }
+    ispis(polje,-5,15);
+    zamjena_dva(polje,1,15)
+    elementi(polje);
     return 0;
+}
