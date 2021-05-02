ec7e0980305c9056bc78c48871bd5ebe4f7a410a
BrunaMariani <bmariani@pmfst.hr>
2021-05-02 22:34:26 +0200

    zdk3

--- vjezbe6/zadatak3.cpp
+++ vjezbe6/zadatak3.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 #include <cmath>
+void ispis(int polje[], int a, int b){
+    int n = sizeof(polje);
+    for (int i = 0; i<21; i++)
+    { 
+        if (polje[i] >= a){
+            if (polje[i] <= b){
+            std::cout << polje[i] << std::endl;
+            }
+        }
+        
+    }
+
+
+
 int main(){
     int polje[20]= {-10,2,34,11,-32,13,56,-23,43,1,0,-5,4,14,-15,16,-17,18,-19,20};
     int a;
