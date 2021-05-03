5913ab1696b98f947dab66ac99863d0b49728cef
BrunaMariani <bmariani@pmfst.hr>
2021-05-03 13:42:57 +0200

    zdk3-ispravljen

--- vjezbe6/zadatak3.cpp
+++ vjezbe6/zadatak3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 
 void ispis(int polje[], int a, int b){
     for (int i = 0; i<20; i++)
@@ -27,7 +28,12 @@ void elementi(int polje[]) {
 int main(){
     int polje[20]= {-10,2,34,11,-32,13,56,-23,43,1,0,-5,4,14,-15,16,-17,18,-19,20};
     ispis(polje,-5,15);
-    zamjena_dva(polje,1,15)
+    zamjena_dva(polje,1,15);
+    elementi(polje);
+    std::sort(std::begin(polje), std::end(polje));
+    elementi(polje);
+
+    std::reverse(std::begin(polje), std::end(polje));
     elementi(polje);
     return 0;
 }
